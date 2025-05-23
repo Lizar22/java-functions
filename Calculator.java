a6ec6ecf93e58a1d4d70ee4c0081bc134cb17e19
Lara Pla <laraplamoreno@gmail.com>
2025-05-23 14:36:19 +0200

    feat: add first method

--- Calculator.java
+++ Calculator.java
@@ -1,59 +1,63 @@
 public class Calculator {
-    
     public static void main(String[] args) {
-
-        //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
-        
+        System.out.println(add(2, 5));
     }
 
-    //Define los métodos basándote en su doc comment.
+    //Ejecuta aquí todos los métodos creados e imprime en la terminal.
 
-    /**
-     * Function name: add
-     * 
-     * @param number1 (double) 
-     * @param number2 (double)
-     * @return (double)
-     * 
-     * Inside the function:
-     * 1. sum 2 numbers
-     */
-    
 
-    /**
-     * Function name: subtract
-     *
-     * @param number1 (double)
-     * @param number2 (double)
-     * @return (double)
-     * 
-     * Inside the function:
-     * 1. subtract 2 numbers
-     */
-    
+//Define los métodos basándote en su doc comment.
 
-    /**
-     * Function name: multiply
-     * 
-     * @param number1 (double)
-     * @param number2 (double)
-     * @return (double)
-     * 
-     * Inside the function:
-     * 1. multiply 2 numbers
-     */
-    
+/**
+ * Function name: add
+ *
+ * @param number1 (double)
+ * @param number2 (double)
+ * @return (double)
+ * <p>
+ * Inside the function:
+ * 1. sum 2 numbers
+ */
+    public static double add(double number1, double number2) {
+        double sum = number1 + number2;
+        return sum;
+    }
 
-    /**
-     * Function name: divide
-     * 
-     * @param number1 (double)
-     * @param number2 (double)
-     * @return (double)
-     * 
-     * Inside the function:
-     * 1. divide 2 numbers
-     */
-    
+/**
+ * Function name: subtract
+ *
+ * @param number1 (double)
+ * @param number2 (double)
+ * @return (double)
+ *
+ * Inside the function:
+ * 1. subtract 2 numbers
+ */
+
+
+/**
+ * Function name: multiply
+ *
+ * @param number1 (double)
+ * @param number2 (double)
+ * @return (double)
+ *
+ * Inside the function:
+ * 1. multiply 2 numbers
+ */
+
+
+
+/**
+ * Function name: divide
+ *
+ * @param number1 (double)
+ * @param number2 (double)
+ * @return (double)
+ *
+ * Inside the function:
+ * 1. divide 2 numbers
+ */
+
 
 }
